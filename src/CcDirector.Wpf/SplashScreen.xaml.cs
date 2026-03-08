250bb0f9f8e1cbb0035f120b927751ba1061ce67
thefrederiksen <soren@duksrevo.com>
2026-03-08 16:49:38 -0400

    style: Redesign splash screen with professional layout
    Replace terminal-style splash with clean professional design:
    left-aligned logo, blue accent line, version/date info,
    and creator credit footer.

--- src/CcDirector.Wpf/SplashScreen.xaml.cs
+++ src/CcDirector.Wpf/SplashScreen.xaml.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Windows;
 
 namespace CcDirector.Wpf;
@@ -7,5 +8,11 @@ public partial class SplashScreen : Window
     public SplashScreen()
     {
         InitializeComponent();
+
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
+        var versionText = version is not null ? $"v{version.Major}.{version.Minor}.{version.Build}" : "v1.0.0";
+
+        VersionText.Text = $"Version {versionText}";
+        DateText.Text = DateTime.Now.ToString("MMMM d, yyyy");
     }
 }
